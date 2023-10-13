de4465168fdc2630055d19734d78d039d1f34fba
Зенченко Евгений <zenchenko_es@outlook.com>
2023-10-13 20:15:18 +0700

    add apscheduler

--- mailing/services.py
+++ mailing/services.py
@@ -14,5 +14,5 @@ scheduler.add_job(job, 'cron', day_of_week='0-6', hour=19, minute=59)
 scheduler.add_job(job, 'cron', month='1-12', day_of_week='0', hour=19, minute=59)
 #раз в год в понедельник в 19-59
 scheduler.add_job(job, 'cron', year='2023', day_of_week='0', hour=19, minute=59)
-1
+
 scheduler.start()
